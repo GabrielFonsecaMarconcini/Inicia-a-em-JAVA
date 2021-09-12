4d05efe32c1fd7f805ccbb289c16baa69629be23
Gabriel Fonseca <gabrielmarconcini25@gmail.com>
2021-09-11 21:25:46 -0300

    Voltando para amarelo

--- Estojo.java
+++ Estojo.java
@@ -2,7 +2,7 @@ public class Estojo {
 
     public static void main (String []args){
 
-                  Caneta c1=new Caneta("Bic","Verde",0.4f);
+                  Caneta c1=new Caneta("Bic","Amarelo",0.4f);
 
                   c1.status();
 
